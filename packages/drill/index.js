f2900640ab1f6d9f264d1774e7fbf5d66873ee08
mateonunez <mateonunez95@gmail.com>
2022-10-27 17:54:28 +0200

    feat(drill): create build database commands

--- packages/drill/index.js
+++ packages/drill/index.js
@@ -1,3 +1,80 @@
-export default function () {
-  return true
+import { generateConnectionString } from './lib/database.js'
+
+export default function setupDatabase (logger, database, options) {
+  const { db, sql } = buildDatabase(logger, database, options)
+
+  return { db, sql }
+}
+
+async function buildDatabase (logger, database, options) {
+  const connectionPool = await resolveConnectionPool(logger, database)
+  const { sql } = connectionPool
+
+  const { [database]: connectionString } = generateConnectionString(options)
+
+  let db = await connect(logger, connectionPool, database, connectionString)
+  db = await resolveMeta(logger, database, db, sql)
+
+  return { db, sql }
+}
+
+async function resolveConnectionPool (logger, database) {
+  if (database === 'mysql') {
+    logger.info('Importing mysql from @databases/mysql')
+    return await (
+      await import('@databases/mysql')
+    ).default
+  } else if (database === 'postgres') {
+    logger.info('Importing postgres from @databases/postgres')
+    return await (
+      await import('@databases/pg')
+    ).default
+  } else if (database === 'sqlite') {
+    logger.info('Importing sqlite from @databases/sqlite')
+    return await (
+      await import('@databases/sqlite')
+    ).default
+  } else {
+    throw new Error(`The database "${database}" is not supported.`)
+  }
+}
+
+function connect (logger, connectionPool, database, connectionString) {
+  logger.info(`Connecting to ${database} database at ${connectionString}`)
+
+  let db
+  try {
+    db = connectionPool({
+      connectionString
+    })
+  } catch (error) {
+    throw new Error(`Could not connect to ${database} database at ${connectionString}`)
+  }
+
+  return db
+}
+
+async function resolveMeta (logger, database, db, sql) {
+  let version
+  let engine
+
+  if (database === 'mysql') {
+    version = (await db.query(sql`SELECT VERSION()`))[0]['VERSION()']
+    engine = 'mysql'
+  } else if (database === 'postgres') {
+    version = await db.query(sql`SELECT VERSION()`)[0][0].version
+    engine = 'postgres'
+  } else if (database === 'sqlite') {
+    version = await db.query(sql`SELECT SQLITE_VERSION()`)[0][0].sqlite_version
+    engine = 'sqlite'
+  } else {
+    throw new Error(`The database "${database}" is not supported.`)
+  }
+
+  db.version = version
+  const isEngine = `is${engine.charAt(0).toUpperCase()}${engine.slice(1)}`
+  db[isEngine] = true
+
+  logger.info(`Meta resolved for ${database} database. Version: ${version}, Engine: ${engine}`)
+  return db
 }
--- packages/drill/lib/database.js
+++ packages/drill/lib/database.js
@@ -1,7 +1,7 @@
 export function generateConnectionString ({ host, port, user, password, databaseName }) {
   return {
-    postgres: `postgres://${user}:${password}@${host}:${port}/${databaseName}`,
-    mysql: `mysql://${user}:${password}@${host}:${port}/${databaseName}`,
+    postgres: `postgres://${user}:${encodeURIComponent(password)}@${host}:${port}/${databaseName}`,
+    mysql: `mysql://${user}:${encodeURIComponent(password)}@${host}:${port}/${databaseName}`,
     sqlite: `sqlite://${databaseName}`
   }
 }
