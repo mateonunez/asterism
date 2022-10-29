4529873a5dcd22f5fbc95a08cd0cfbfc9a6d3693
mateonunez <mateonunez95@gmail.com>
2022-10-29 23:08:58 +0200

    feat: create version cjs

--- packages/cli/index.mjs
+++ packages/cli/index.mjs
@@ -1,13 +1,9 @@
 #!/usr/bin/env node
 
 import { program } from 'commander'
-import { readFileSync } from 'node:fs'
-import { resolve } from 'node:path'
+import version from './lib/version.js'
 import { allowedDatabases } from '@asterism/drill/lib/database.js'
 import rocket from '@asterism/rocket'
-
-const root = new URL('.', import.meta.url).pathname.replace(/\/$/, '')
-const { version } = JSON.parse(readFileSync(resolve(root, '../../package.json'), 'utf8'))
 
 program.name('asterism').description('Asterism CLI').version(version, '-v, --version', 'output the current version')
 
