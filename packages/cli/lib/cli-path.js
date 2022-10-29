49dc3f1209f147632ad0283d019d6f7d121948bc
mateonunez <mateonunez95@gmail.com>
2022-10-29 23:29:36 +0200

    chore: rename

--- packages/cli/lib/cli-path.js
+++ packages/cli/lib/cli-path.js
@@ -1,6 +1,6 @@
 const { resolve, join } = require('path')
 
 const root = resolve(__dirname, '../')
-const cliPath = join(root, 'index.mjs')
+const cliPath = join(root, 'cli.mjs')
 
 module.exports = cliPath
