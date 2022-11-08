925c077109c271ee3bac52dace9db82328fd2e5e
mateonunez <mateonunez95@gmail.com>
2022-11-08 10:13:38 +0100

    perf: add getTables method on mysql and pg

--- packages/drill/lib/queries/mysql.js
+++ packages/drill/lib/queries/mysql.js
@@ -2,6 +2,21 @@ import common from './common.js'
 
 export default async function (logger, db, sql) {
   return {
-    ...(await common(logger, db, sql))
+    ...(await common(logger, db, sql)),
+    getTables: () => getTables(logger, db, sql)
   }
+}
+
+async function getTables (logger, db, sql) {
+  const response = await db.query(sql`
+    SELECT TABLE_NAME
+    FROM information_schema.tables
+    WHERE table_schema = (SELECT DATABASE())
+  `)
+
+  const tables = []
+  for (const { TABLE_NAME } of response) {
+    tables.push(TABLE_NAME)
+  }
+  return tables
 }
--- packages/drill/lib/queries/postgres.js
+++ packages/drill/lib/queries/postgres.js
@@ -2,6 +2,26 @@ import common from './common.js'
 
 export default async function (logger, db, sql) {
   return {
-    ...common
+    ...(await common(logger, db, sql)),
+    getTables: () => getTables(logger, db, sql)
   }
+}
+
+async function getTables (logger, db, sql) {
+  // Get tables on Postgres
+  if (logger) logger.info('Getting tables.')
+  const response = await db.query(sql`
+    SELECT table_name
+    FROM information_schema.tables
+    WHERE table_schema = 'public'
+    AND table_type = 'BASE TABLE'
+  `)
+  if (response.length === 0) {
+    if (logger) logger.warn('No tables found.')
+  }
+  const tables = []
+  for (const { table_name: tableName } of response) {
+    tables.push(tableName)
+  }
+  return tables
 }
