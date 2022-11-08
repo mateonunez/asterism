7af139b0112fb12eca2f3b327221476d3bf79952
mateonunez <mateonunez95@gmail.com>
2022-11-08 19:19:54 +0100

    test(drill): add basic test

--- packages/drill/test/index.test.js
+++ packages/drill/test/index.test.js
@@ -1,5 +1,14 @@
 import { test } from 'tap'
+import setupDatabase, { killDatabase } from '../drill.js'
+import huston from '@asterism/huston'
 
-test('rocket', async ({ ok }) => {
-  ok(true)
+const { mysqlOptions } = huston
+
+test('the database should setup correctly', async ({ teardown, ok }) => {
+  const { db, queryer } = await setupDatabase(undefined, 'mysql', mysqlOptions)
+
+  ok(db)
+  ok(queryer)
+
+  teardown(() => killDatabase(db))
 })
