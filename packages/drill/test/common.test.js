5fbf2f188a68a198de95825d040778808c19f7cb
mateonunez <mateonunez95@gmail.com>
2022-11-12 12:14:21 +0100

    test(drill): implement logger mock

--- packages/drill/test/common.test.js
+++ packages/drill/test/common.test.js
@@ -1,10 +1,9 @@
 import { test } from 'tap'
 import setupDatabase, { killDatabase } from './../drill.js'
-import logger from './helpers/logger.js'
 import symbols from '../lib/symbols.js'
 import huston from '@mateonunez/asterism-huston'
 
-const { mysqlOptions, postgresOptions } = huston
+const { mysqlOptions, postgresOptions, logger } = huston
 const { privateMethods } = symbols
 
 async function createTable (queryer, tableName) {
