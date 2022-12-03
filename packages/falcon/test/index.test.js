9c28c8a8206090c9923a78b649a1c5dd270d68ca
mateonunez <mateonunez95@gmail.com>
2022-12-03 18:25:43 +0100

    test(falcon): edit options on migrate and search methods

--- packages/falcon/test/index.test.js
+++ packages/falcon/test/index.test.js
@@ -6,31 +6,31 @@ import huston from '@mateonunez/asterism-huston'
 const { mysqlOptions, postgresOptions } = huston
 
 t.before(async () => {
-  await seeder(undefined, 'mysql', mysqlOptions)
+  await seeder(undefined, 'mysql', { ...mysqlOptions, outputDir: './lyra' })
 })
 
 t.test('no database selected should default to mysql', async ({ ok }) => {
-  const { db } = await falconMigrate(undefined, mysqlOptions)
+  const { db } = await falconMigrate(undefined, { ...mysqlOptions, outputDir: './lyra' })
   ok(db.isMysql)
 })
 
 t.test('postgres database selected', async ({ ok }) => {
-  const { db } = await falconMigrate('postgres', postgresOptions)
+  const { db } = await falconMigrate('postgres', { ...postgresOptions, outputDir: './lyra' })
   ok(db.isPostgres)
 })
 
 t.test('falcon search with no term should terminate the process', async ({ same }) => {
   await falconMigrate(undefined, mysqlOptions)
-  const results = await falconSearch(undefined, { outputDir: './out' })
+  const results = await falconSearch(undefined, { inputDir: './lyra' })
   same(results, undefined)
 })
 
 t.test('falcon search should return results', async ({ ok }) => {
   await falconMigrate(undefined, mysqlOptions)
-  const results = await falconSearch('John', mysqlOptions)
+  const results = await falconSearch('John', { inputDir: './lyra' })
   ok(results)
 })
 
 t.on('end', async () => {
-  await dropSeed(undefined, 'mysql', mysqlOptions)
+  await dropSeed(undefined, 'mysql', { ...mysqlOptions, outputDir: './lyra' })
 })
