148f2ac683df22e6c5b0cbbe59ca170a1619a4a4
mateonunez <mateonunez95@gmail.com>
2023-03-02 00:24:31 +0100

    test(drill): more improvements

--- packages/drill/test/database.test.js
+++ packages/drill/test/database.test.js
@@ -5,14 +5,14 @@ import { database } from '@mateonunez/asterism-huston'
 const { mysqlOptions, postgresOptions } = database
 
 test('should generate correctly the connection string', ({ end }) => {
-  test('mysql', async ({ same }) => {
+  test('mysql', async ({ equal }) => {
     const { mysql } = generateConnectionString(mysqlOptions)
-    same(mysql, 'mysql://root:toor@127.0.0.1:3306/db')
+    equal(mysql, 'mysql://root:toor@127.0.0.1:3306/db')
   })
 
-  test('postgres', async ({ same }) => {
+  test('postgres', async ({ equal }) => {
     const { postgres } = generateConnectionString(postgresOptions)
-    same(postgres, 'postgres://postgres:toor@127.0.0.1:5432/db')
+    equal(postgres, 'postgres://postgres:toor@127.0.0.1:5432/db')
   })
 
   end()
--- packages/drill/test/errors.test.js
+++ packages/drill/test/errors.test.js
@@ -1,18 +1,10 @@
 import { test } from 'tap'
 import setupDatabase from '../drill.js'
 
-test('should throw an error when the database is not support', async ({ ok }) => {
-  try {
-    await setupDatabase(null, 'oracle', {})
-  } catch (err) {
-    ok(err)
-  }
+test('should throw an error when the database is not support', async ({ rejects }) => {
+  await rejects(setupDatabase(null, 'fake', {}), 'Database not supported')
 })
 
-test('should throw an error when the parameters are wront', async ({ ok }) => {
-  try {
-    await setupDatabase(null, 'mysql', { user: 'fake', password: 'kaboom' })
-  } catch (err) {
-    ok(err)
-  }
+test('should throw an error when the parameters are wront', async ({ rejects }) => {
+  await rejects(setupDatabase(null, 'mysql', { user: 'fake', password: 'kaboom' }), 'Error connecting to database')
 })
