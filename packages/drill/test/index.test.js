6cbff4ef51283ba915799af72c9b41cb154cfd85
mateonunez <mateonunez95@gmail.com>
2023-03-02 01:10:41 +0100

    test(drill): improve index tests

--- packages/drill/test/index.test.js
+++ packages/drill/test/index.test.js
@@ -1,11 +1,15 @@
 import { test, beforeEach, afterEach } from 'tap'
 import setupDatabase, { killDatabase, resolveTables, resolveData } from '../drill.js'
 import { database, logger } from '@mateonunez/asterism-huston'
+import symbols from '../lib/symbols.js'
+import { allowedDatabases } from '../lib/database.js'
 
+const { privateMethods } = symbols
 const { mysqlOptions, postgresOptions } = database
 
 let dbMysql, queryerMysql
 let dbPostgres, queryerPostgres
+
 beforeEach(async () => {
   const { db: _dbMysql, queryer: _queryerMysql } = await setupDatabase(logger, 'mysql', mysqlOptions)
   dbMysql = _dbMysql
@@ -17,69 +21,104 @@ beforeEach(async () => {
 })
 
 afterEach(async () => {
+  queryerMysql[privateMethods].dropDatabase(mysqlOptions.databaseName)
+  queryerPostgres[privateMethods].dropDatabase(postgresOptions.databaseName)
   await killDatabase(dbMysql)
   await killDatabase(dbPostgres)
 })
 
-test('the database should setup correctly', async ({ end }) => {
-  test('mysql', async ({ ok }) => {
-    ok(dbMysql)
-    ok(queryerMysql)
-  })
+function getObjects (database) {
+  return database === 'mysql' ? { db: dbMysql, queryer: queryerMysql } : database === 'postgres' ? { db: dbPostgres, queryer: queryerPostgres } : {}
+}
 
-  test('postgres', async ({ ok }) => {
-    ok(dbPostgres)
-    ok(queryerPostgres)
+function getOptions (database) {
+  return database === 'mysql' ? mysqlOptions : database === 'postgres' ? postgresOptions : {}
+}
+
+test('the database should setup correctly', async ({ end }) => {
+  allowedDatabases.forEach((supportedDatabase) => {
+    test(supportedDatabase, async ({ ok }) => {
+      const { db, queryer } = getObjects(supportedDatabase)
+      ok(db)
+      ok(queryer)
+    })
   })
 
   end()
 })
 
 test('should resolve all the tables', ({ end }) => {
-  test('mysql', async ({ ok }) => {
-    const tables = await resolveTables(logger, queryerMysql, mysqlOptions)
+  allowedDatabases.forEach((supportedDatabase) => {
+    test(supportedDatabase, async ({ same }) => {
+      const { queryer } = getObjects(supportedDatabase)
+      const tables = await resolveTables(logger, queryer, getOptions(supportedDatabase))
 
-    ok(tables)
-  })
-
-  test('postgres', async ({ ok }) => {
-    const tables = await resolveTables(logger, queryerPostgres, postgresOptions)
-
-    ok(tables)
+      same(tables, ['test'])
+    })
   })
 
   end()
 })
 
 test('should resolve single table', ({ end }) => {
-  test('mysql', async ({ ok }) => {
-    const tables = await resolveTables(logger, queryerMysql, { mysqlOptions, tableName: 'users' })
+  allowedDatabases.forEach((supportedDatabase) => {
+    test(supportedDatabase, async ({ same, teardown }) => {
+      const { queryer } = getObjects(supportedDatabase)
+      await queryer[privateMethods].createTable('users', {
+        id: {
+          type: 'int',
+          primaryKey: true
+        }
+      })
 
-    ok(tables)
+      const tables = await resolveTables(logger, queryer, { ...getOptions(supportedDatabase), tableName: 'users' })
+      await queryer[privateMethods].dropTable('users')
+
+      same(tables, ['users'])
+    })
   })
 
-  test('postgres', async ({ ok }) => {
-    const tables = await resolveTables(logger, queryerPostgres, { ...postgresOptions, tableName: 'users' })
+  end()
+})
 
-    ok(tables)
+test('should resolve single table even if it not exists', ({ end }) => {
+  allowedDatabases.forEach((supportedDatabase) => {
+    test(supportedDatabase, async ({ same, teardown }) => {
+      const { queryer } = getObjects(supportedDatabase)
+
+      const tables = await resolveTables(logger, queryer, { ...getOptions(supportedDatabase), tableName: 'users' })
+
+      same(tables, [null])
+    })
   })
 
   end()
 })
 
 test('should resolve data', ({ end }) => {
-  test('mysql', async ({ ok }) => {
-    const tables = await resolveTables(logger, queryerMysql, mysqlOptions)
-    const data = await resolveData(logger, queryerMysql, tables, mysqlOptions)
+  allowedDatabases.forEach((supportedDatabase) => {
+    test(supportedDatabase, async ({ same }) => {
+      const { queryer } = getObjects(supportedDatabase)
+      await queryer[privateMethods].createTable('users', {
+        name: {
+          type: 'varchar',
+          length: 255
+        }
+      }, { dropIfExists: true })
 
-    ok(data)
-  })
+      await queryer[privateMethods].insertData('users', { name: 'John' })
 
-  test('postgres', async ({ ok }) => {
-    const tables = await resolveTables(logger, queryerPostgres, postgresOptions)
-    const data = await resolveData(logger, queryerPostgres, tables, postgresOptions)
+      const tables = await resolveTables(logger, queryer, { ...getOptions(supportedDatabase), tableName: 'users' })
+      const data = await resolveData(logger, queryer, tables, { ...getOptions(supportedDatabase), tableName: 'users' })
 
-    ok(data)
+      await queryer[privateMethods].dropTable('users')
+
+      same(data, {
+        users: [{
+          name: 'John'
+        }]
+      })
+    })
   })
 
   end()
